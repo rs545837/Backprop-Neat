bf07a888256da164ba48cdd857ffecca900dd760
rs545837 <114828377+rs545837@users.noreply.github.com>
2025-01-18 10:54:50 -0800

    Update main_v3.py

--- main_v3.py
+++ main_v3.py
@@ -60,24 +60,12 @@ def identity(x):
 
 fun_enum = [identity, jnp.absolute, jnp.square, jnp.sin, jnp_relu, neat_act]
 
-def jaccard_sim(list1, list2):
-    set1, set2 = set(list1), set(list2)
-    intersection = len(set1.intersection(set2))
-    union = len(set1.union(set2))
-    return intersection / union if union != 0 else 0
-
 def getActivations(genelist):
     activation_list = np.zeros(MAX_NODE_CT, dtype=int)
     for gene in genelist:
         if gene.enable:
             activation_list[gene.out_node] = gene.activation
     return activation_list
-
-def wt_init():
-    wt = np.random.normal(scale=0.01)
-    while wt == 0:
-        wt = np.random.normal(scale=0.01)
-    return wt
 
 
 def visualize_graph(adj_mat, gen_num, fitness, activations, is_best=False):
@@ -193,19 +181,6 @@ class Gene:
         self.innov_num = innov_num
         self.activation = activation
 
-def topoSort(graph):
-    arr = []
-    theset = set()
-    def DFS(n):
-        if n not in theset:
-            theset.add(n)
-            for i, x in enumerate(graph[n]):
-                if x != 0:
-                    DFS(i)
-            arr.append(n)
-    for i in range(num_in):
-        DFS(i)
-    return np.array(arr)[::-1]
 
 def geneGraph(genelist):
     graph = np.zeros((MAX_NODE_CT, MAX_NODE_CT))
